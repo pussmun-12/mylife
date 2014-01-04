e4ac8164cc372fdecb4f797b1bec671f3a4d5272
pussmun-12 <hakan.json@gmail.com>
2014-01-05 00:13:45 +0100

    jhjhj

--- src/org/apache/cordova/plugin/Echo.java
+++ src/org/apache/cordova/plugin/Echo.java
@@ -6,26 +6,63 @@ import org.apache.cordova.api.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import java.io.*;
+import java.util.*;
+import 	android.os.Environment;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class Echo extends CordovaPlugin {
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("echo")) {
-            String message = args.getString(0); 
-            this.echo(message, callbackContext);
-            return true;
-        }
-        return false;
-    }
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		if (action.equals("echo")) {
+			String message = args.getString(0); 
+			this.echo(message, callbackContext);
+			return true;
+		}
+		return false;
+	}
 
-    private void echo(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) { 
-            callbackContext.success(message + " fucker!");
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
-    }
+	private void echo(String message, CallbackContext callbackContext) {
+		if (message != null && message.length() > 0) {
+
+
+
+			try{
+				callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, getImgPaths()));
+			}
+			catch(JSONException e){
+				callbackContext.error("ERROR!");
+			}
+		} else {
+			callbackContext.error("Expected one non-empty string argument.");
+		}
+	}
+
+	private JSONObject getImgPaths() throws JSONException {
+		File file[] = Environment.getExternalStorageDirectory().listFiles();
+		JSONObject toReturn = new JSONObject();
+		recursiveFileFind(file, toReturn);
+		return toReturn;
+	}
+
+	public void recursiveFileFind(File[] file1, JSONObject toReturn) throws JSONException{
+		int i = 0;
+		String filePath="";
+		if(file1!=null){
+			while(i!=file1.length){
+				filePath = file1[i].getAbsolutePath();
+				if(file1[i].isDirectory()){
+					File file[] = file1[i].listFiles();
+					recursiveFileFind(file, toReturn);
+				}
+				else{
+					toReturn.put(filePath, filePath);
+				}
+				i++;
+				//Log.d(i+"", filePath);
+			}
+		}
+	}
 }
